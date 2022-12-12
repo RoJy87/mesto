02f157e4740fc650e7c6546cd838efa4e2b4dd04
Danil Simonov <simdanila@gmail.com>
2022-12-13 02:45:53 +0300

    рефакторинг JS

--- script/script.js
+++ script/script.js
@@ -5,21 +5,17 @@ const closeButton = popup.querySelector('.popup__close-btn');
 
 
 function openPopup() {
-    editButton.addEventListener('click', function () {
-        popup.classList.add('popup_opened');
-        page.style.overflow = 'hidden';
-    });
+  popup.classList.add('popup_opened');
+  page.style.overflow = 'hidden';
 }
 
 function closePopup() {
-    closeButton.addEventListener('click', function () {
-        popup.classList.remove('popup_opened');
-        page.style.overflow = 'auto';
-    });
+  popup.classList.remove('popup_opened');
+  page.style.overflow = 'auto';
 }
 
-openPopup();
-closePopup();
+editButton.addEventListener('click', openPopup);
+closeButton.addEventListener('click', closePopup);
 
 // Находим форму в DOM
 let formElement = document.querySelector('.popup__form-data'); // Воспользуйтесь методом querySelector()
@@ -27,23 +23,23 @@ let formElement = document.querySelector('.popup__form-data'); // Восполь
 // Обработчик «отправки» формы, хотя пока
 // она никуда отправляться не будет
 function formSubmitHandler(evt) {
-    evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
-    // Так мы можем определить свою логику отправки.
-    // О том, как это делать, расскажем позже.
+  evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
+  // Так мы можем определить свою логику отправки.
+  // О том, как это делать, расскажем позже.
 
-    // Находим поля формы в DOM
-    let nameInput = document.querySelector('.popup__form-name').value; // Воспользуйтесь инструментом .querySelector()
-    let jobInput = document.querySelector('.popup__form-status').value; // Воспользуйтесь инструментом .querySelector()
+  // Находим поля формы в DOM
+  let nameInput = document.querySelector('.popup__form-name').value; // Воспользуйтесь инструментом .querySelector()
+  let jobInput = document.querySelector('.popup__form-status').value; // Воспользуйтесь инструментом .querySelector()
 
-    let newHeader = document.querySelector('.profile__name');
-    let newDescription = document.querySelector('.profile__description');  // Выберите элементы, куда должны быть вставлены значения полей
+  let newHeader = document.querySelector('.profile__name');
+  let newDescription = document.querySelector('.profile__description');  // Выберите элементы, куда должны быть вставлены значения полей
 
 
-    newHeader.textContent = nameInput;
-    newDescription.textContent = jobInput;// Вставьте новые значения с помощью textContent
+  newHeader.textContent = nameInput;
+  newDescription.textContent = jobInput;// Вставьте новые значения с помощью textContent
 
-    popup.classList.remove('popup_opened');
-    page.style.overflow = 'auto';
+  popup.classList.remove('popup_opened');
+  page.style.overflow = 'auto';
 }
 
 // Прикрепляем обработчик к форме:
